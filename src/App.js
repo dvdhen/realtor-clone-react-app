23b6566fdcf6c01f5ccbcc7c3d982c3ffbe175a3
unknown <egwa1@outlook.com>
2022-11-09 22:17:38 +0100

    create pagges and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <div>
-     <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+     <Route path = "/" element = {<Home/>} />
+     <Route path = "/profile" element = {<Profile/>} />
+     <Route path = "/sign-in" element = {<SignIn/>} />
+     <Route path = "/sign-up" element = {<SignUp/>} />
+     <Route path = "/offers" element = {<Offers/>} />
+     <Route path = "/pass-word" element = {<ForgotPassword/>} />
+     </Routes>
+    </Router>
   );
 }
 
